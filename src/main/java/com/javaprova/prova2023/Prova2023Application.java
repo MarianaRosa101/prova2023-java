c711565ddd3de24ee655fa0bcabbde143f1c81de
MarianaRosa101 <marianarosa100@gmail.com>
2023-06-20 10:35:50 -0300

    commit

---
+++ src/main/java/com/javaprova/prova2023/Prova2023Application.java
@@ -0,0 +1,13 @@
+package com.javaprova.prova2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaprova/prova2023/Prova2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprova.prova2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
